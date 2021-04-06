ac97436eb7a92e9ec4b1a41c6337a7397158f6c8
Patrick Organ <patrick@permafrost.dev>
2021-04-06 16:22:02 -0400

    apply formatting

--- tests/issues/RepositoryIssue.test.ts
+++ tests/issues/RepositoryIssue.test.ts
@@ -8,13 +8,10 @@ it('resolves and sets the name of the fixer', () => {
 
     const issue = new RepositoryIssue({ kind: 'missing_pkg', score: 0 }, 'test', null, null, skeleton, repo);
 
-    expect(issue.resolved)
-        .toBeFalsy();
+    expect(issue.resolved).toBeFalsy();
 
     issue.resolve('FixerName');
 
-    expect(issue.resolved)
-        .toBeTruthy();
-    expect(issue.resolvedByFixer)
-        .toBe('FixerName');
+    expect(issue.resolved).toBeTruthy();
+    expect(issue.resolvedByFixer).toBe('FixerName');
 });
--- tests/lib/helpers.test.ts
+++ tests/lib/helpers.test.ts
@@ -3,26 +3,19 @@
 import { isDirectory, last, uniqueArray, uniqueStrings } from '../../src/lib/helpers';
 
 it('determines if a filesystem item is a directory', () => {
-    expect(isDirectory(__dirname))
-        .toBeTruthy();
-    expect(isDirectory(__filename))
-        .toBeFalsy();
+    expect(isDirectory(__dirname)).toBeTruthy();
+    expect(isDirectory(__filename)).toBeFalsy();
 });
 
 it('ensures an array has only unique items', () => {
-    expect(uniqueArray([1, 1, 2, 2, 3]))
-        .toStrictEqual([1, 2, 3]);
-    expect(uniqueArray([1, 1, 1, 1]))
-        .toStrictEqual([1]);
-    expect(uniqueArray([1, 2]))
-        .toStrictEqual([1, 2]);
+    expect(uniqueArray([1, 1, 2, 2, 3])).toStrictEqual([1, 2, 3]);
+    expect(uniqueArray([1, 1, 1, 1])).toStrictEqual([1]);
+    expect(uniqueArray([1, 2])).toStrictEqual([1, 2]);
 });
 
 it('ensures an array of strings has unique values', () => {
-    expect(uniqueStrings(['aa', 'aa', 'bb']))
-        .toStrictEqual(['aa', 'bb']);
-    expect(uniqueStrings(['aa', '', 'bb', '']))
-        .toStrictEqual(['aa', '', 'bb', '']);
+    expect(uniqueStrings(['aa', 'aa', 'bb'])).toStrictEqual(['aa', 'bb']);
+    expect(uniqueStrings(['aa', '', 'bb', ''])).toStrictEqual(['aa', '', 'bb', '']);
 });
 
 it('gets the last item of an array', () => {
@@ -31,10 +24,7 @@ it('gets the last item of an array', () => {
         two: ['one', 'two', 'three'],
     };
 
-    expect(last(data.one))
-        .toBe(3);
-    expect(last(data.two))
-        .toBe('three');
-    expect(last([]))
-        .toBeUndefined();
+    expect(last(data.one)).toBe(3);
+    expect(last(data.two)).toBe('three');
+    expect(last([])).toBeUndefined();
 });
