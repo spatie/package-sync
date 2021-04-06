44b8d72f9488a9d6d86736e213201f9555a3ff37
Patrick Organ <patrick@permafrost.dev>
2021-04-06 01:10:48 -0400

    syntax fix

--- src/commands/Fix.ts
+++ src/commands/Fix.ts
@@ -16,15 +16,6 @@ export default class FixCommand extends Command {
     public static options = [createOption('fixer', undefined, { alias: 'F', type: 'string' })];
 
     static handle(argv: any): void {
-        // const skeletonType = argv.packageName.startsWith('laravel-') ? 'laravel' : 'php';
-        // const templateName = app.configuration.getFullTemplateName(skeletonType);
-        // const skeletonPath = app.templatePath(templateName);
-        // const repositoryPath = app.packagePath(argv.packageName);
-
-        // console.log('Analyzing package: ' + argv.packageName);
-
-        // const results = app.compareDotFiles(skeletonPath, repositoryPath);
-
         let issueType = argv.issueType;
 
         if (issueType.trim().length === 0) {
@@ -52,12 +43,6 @@ export default class FixCommand extends Command {
                 .forEach(fixer => {
                     if (fixer.fixes(issue.kind) && fixer.canFix(issue)) {
                         issue.availableFixers.push(fixer.prettyName());
-                    //     if ((issue.note?.length ?? 0) > 0) {
-                    //     //
-                    //     } else {
-                    //         issue.note = 'fix available';
-                    //     }
-                    //     issue.note += ' ' + fixer.prettyName();
                     }
                 });
         });
