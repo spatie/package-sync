eefe69a9a9f588e70364c3e9566aa3f2869a4b0f
Patrick Organ <patrick@permafrost.dev>
2021-04-07 18:21:59 -0400

    remove development warning msg on startup, change yargs script name to package-sync

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import { loadCommand } from './commands/Command';
-import { init } from './lib/PrototypesInit';
-
-init();
 
 const yargs = require('yargs');
 
-console.log(`\n*** READ THE README FIRST TO AVOID UNEXPECTED FILE CHANGES ***`);
-
 yargs(process.argv.slice(2))
-    //.scriptName('spatie-package')
+    .scriptName('package-sync')
     .version('1.0.0')
     .command(loadCommand(require('./commands/Analyze')))
     .command(loadCommand(require('./commands/Fix')))
+    .command(loadCommand(require('./commands/ListFixers')))
     .command(loadCommand(require('./commands/PullTemplate')))
     .command(loadCommand(require('./commands/PullPackage')))
-    .command(loadCommand(require('./commands/ListFixers')))
     .demandCommand()
     .help()
     .wrap(120).argv;
-
-//
