68e3b1b23bcbc8e8713fbf50d230debe6e379a26
Patrick Organ <patrick@permafrost.dev>
2021-04-07 23:46:08 -0400

    add tests wip

---
+++ tests/lib/FileMerger.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+
+import { FileMerger } from '../../src/lib/FileMerger';
+
+it('merges two text files together', () => {
+    const fm = FileMerger.create()
+        .add(`${__dirname}/../data/test-package-2/one.txt`)
+        .add(`${__dirname}/../data/test-package-2/two.txt`);
+
+    expect(fm.merge())
+        .toMatchSnapshot();
+});
+
+it('merges the same text file twice correctly', () => {
+    const fm = FileMerger.create()
+        .add(`${__dirname}/../data/test-package-2/one.txt`)
+        .add(`${__dirname}/../data/test-package-2/one.txt`);
+
+    expect(fm.merge())
+        .toBe(`one\nabc`);
+});
