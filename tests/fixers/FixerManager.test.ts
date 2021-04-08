034fe73e81c51f8397ebdcefd3def99f6be152f6
Patrick Organ <patrick@permafrost.dev>
2021-04-08 06:26:00 -0400

    additional tests

--- tests/fixers/FixerManager.test.ts
+++ tests/fixers/FixerManager.test.ts
@@ -2,6 +2,7 @@
 import { FixerManager } from '../../src/fixers/FixerManager';
 import { DirectoryNotFoundFixer } from '../../src/fixers/DirectoryNotFoundFixer';
 import { FileNotFoundFixer } from '../../src/fixers/FileNotFoundFixer';
+import { Configuration } from '../../src/Configuration';
 
 it('gets the class of a fixer based on its name', () => {
     const fixers = [
@@ -9,8 +10,24 @@ it('gets the class of a fixer based on its name', () => {
         FixerManager.getFixerClass(DirectoryNotFoundFixer.prettyName()),
     ];
 
-    expect(fixers[0]).toBe(FileNotFoundFixer);
-    expect(fixers[1]).toBe(DirectoryNotFoundFixer);
-    expect(FixerManager.getFixerClass('missing')).toBeNull();
-    expect(FixerManager.getFixerClass('')).toBeNull();
+    expect(fixers[0])
+        .toBe(FileNotFoundFixer);
+    expect(fixers[1])
+        .toBe(DirectoryNotFoundFixer);
+    expect(FixerManager.getFixerClass('missing'))
+        .toBeNull();
+    expect(FixerManager.getFixerClass(''))
+        .toBeNull();
+});
+
+it('correctly checks if a fixer is disabled', () => {
+    const config = new Configuration(__dirname + '/../data/index.yml').conf;
+    config.fixers.disabled = ['create-file'];
+
+    const fm = new FixerManager(config);
+
+    expect(fm.isFixerDisabled(FileNotFoundFixer))
+        .toBeTruthy();
+    expect(fm.isFixerDisabled(DirectoryNotFoundFixer))
+        .toBeFalsy();
 });
